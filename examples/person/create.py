0cda486e39c7f8953e92c22bcab73bbe5e6646c3
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-25 20:48:02 -0300

    adiciona exemplos

---
+++ examples/person/create.py
@@ -0,0 +1,15 @@
+from imopay_wrapper import ImopayWrapper
+
+client = ImopayWrapper()
+
+p = {
+    "email": "tyler13@example.net",
+    "phone": "+55 (019) 92498 1907",
+    "first_name": "William",
+    "last_name": "Clark",
+    "cpf": "63304280693",
+    "birthdate": "1947-04-11",
+    "mobile_phone": "+55 60 97852 2366",
+}
+
+response = client.person.create(p)
