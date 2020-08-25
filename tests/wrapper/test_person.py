3dc21d41dd6150d6e33b2bb9091566cc6257bc2f
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-25 18:25:22 -0300

    incrementa teste

--- tests/wrapper/test_person.py
+++ tests/wrapper/test_person.py
@@ -4,30 +4,25 @@ from factories.person import PersonFactory, Person
 
 
 class PersonWrapperTestCase(LocalImopayTestCase):
-    def test_create(self):
+    def test_create_update_retrieve(self):
         client = PersonWrapper()
 
-        p = PersonFactory()
+        p = PersonFactory(first_name="original")
 
         response = client.create_person(p.to_dict())
 
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.get("first_name"), "original")
 
-    def test_update_put(self):
-        client = PersonWrapper()
+        self.id = response.data.get("id")
 
-        p = PersonFactory(first_name='original')
+        p = Person.from_dict({"first_name": "modificado"})
 
-        response = client.create_person(p.to_dict())
-
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data.get('first_name'), 'original')
-
-        id = response.data.get('id')
-
-        p = Person.from_dict({'first_name': 'modificado'})
-
-        response = client.update_person(id, p.to_dict())
+        response = client.update_person(self.id, p.to_dict())
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.get('first_name'), 'modificado')
+        self.assertEqual(response.data.get("first_name"), "modificado")
+
+        response = client.retrieve_person(self.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get("first_name"), "modificado")
