a001528e224936f91eba84f421cc06611a1e1901
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-25 18:18:52 -0300

    turbina testes

--- tests/wrapper/test_person.py
+++ tests/wrapper/test_person.py
@@ -1,13 +1,33 @@
-from unittest import TestCase
-
+from ..utils import LocalImopayTestCase
 from imopay_wrapper.wrapper.person import PersonWrapper
-from factories.person import PersonFactory
+from factories.person import PersonFactory, Person
 
 
-class PersonWrapperTestCase(TestCase):
+class PersonWrapperTestCase(LocalImopayTestCase):
     def test_create(self):
         client = PersonWrapper()
 
         p = PersonFactory()
 
-        client.create_person(p.to_dict())
+        response = client.create_person(p.to_dict())
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_update_put(self):
+        client = PersonWrapper()
+
+        p = PersonFactory(first_name='original')
+
+        response = client.create_person(p.to_dict())
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.get('first_name'), 'original')
+
+        id = response.data.get('id')
+
+        p = Person.from_dict({'first_name': 'modificado'})
+
+        response = client.update_person(id, p.to_dict())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get('first_name'), 'modificado')
