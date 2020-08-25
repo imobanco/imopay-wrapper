5557c6ce8c2adb0e435ad9412a11f85610819d80
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-25 20:12:25 -0300

    PersonWrapper com atributos model & action

--- imopay_wrapper/wrapper/person.py
+++ imopay_wrapper/wrapper/person.py
@@ -7,16 +7,10 @@ class PersonWrapper(BaseImopayWrapper):
     Wrapper para os métodos de person
     """
 
-    def create_person(self, data: dict):
-        p = Person.from_dict(data)
-        url = self._construct_url(action="persons")
-        return self._post(url, p.to_dict())
+    @property
+    def model(self):
+        return Person
 
-    def update_person(self, identifier: str, data: dict):
-        p = Person.from_dict(data)
-        url = self._construct_url(action="persons", identifier=identifier)
-        return self._patch(url, p.to_dict())
-
-    def retrieve_person(self, identifier: str):
-        url = self._construct_url(action="persons", identifier=identifier)
-        return self._get(url)
+    @property
+    def action(self):
+        return 'persons'
