171c2bf1ebf577be56439e402d6c012bf44ffdc5
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-25 21:20:13 -0300

    adiciona exemplos de address

---
+++ examples/address/create.py
@@ -0,0 +1,17 @@
+from imopay_wrapper import ImopayWrapper
+
+client = ImopayWrapper()
+
+a = {
+    "owner": "algum id",
+    "city": "Natal",
+    "state": "RN",
+    "zip_code": "99999999",
+    "street": "Peterson Mills",
+    "number": "9626",
+    "neighborhood": "Crescent",
+    "complement": "Senior condition research. City strategy such start. Us brother back against true fall.",
+}
+
+
+response = client.address.create(a)
