046b7af5eed0bf1ef2a17cb2aff1d661ea102919
Rodrigo Castro <rodrigondec@gmail.com>
2020-08-29 22:30:22 -0300

    adiciona métodos de busca de person/company

--- imopay_wrapper/wrapper/company.py
+++ imopay_wrapper/wrapper/company.py
@@ -14,3 +14,10 @@ class CompanyWrapper(BaseImopayWrapper):
     @property
     def action(self):
         return "companies"
+
+    def search(self, value):
+        data = {
+            "cnpj": value
+        }
+        url = self._construct_url(action=self.action, subaction='search')
+        return self._post(url, data)
--- tests/wrapper/test_company.py
+++ tests/wrapper/test_company.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from ..utils import LocalImopayTestCase
 from imopay_wrapper import ImopayWrapper
 from imopay_wrapper.models.company import Company
@@ -12,3 +14,22 @@ class CompanyWrapperTestCase(LocalImopayTestCase):
 
     def test_action(self):
         self.assertEqual(self.client.action, "companies")
+
+    def test_search(self):
+        cnpj = 'foo'
+
+        expected_data = {
+            "cnpj": cnpj
+        }
+
+        expected_url = self.client._construct_url(action=self.client.action, subaction='search')
+
+        with patch(
+            "imopay_wrapper.wrapper.base.BaseImopayWrapper._post"
+        ) as mocked_post:
+            self.client.search(cnpj)
+
+        mocked_post.assert_called_once_with(
+            expected_url,
+            expected_data
+        )
