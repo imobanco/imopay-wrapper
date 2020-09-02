fe5f3ba32078ed13c54e662a0f7f30d80b62e883
Rodrigo Castro <rodrigondec@gmail.com>
2020-09-02 11:29:08 -0300

    adiciona testes

--- tests/wrapper/test_address.py
+++ tests/wrapper/test_address.py
@@ -37,3 +37,48 @@ class PersonWrapperTestCase(LocalImopayTestCase):
             ),
             mocked_model.return_value.return_value.to_dict.return_value,
         )
+
+    def test_update(self):
+        with patch(
+            "imopay_wrapper.wrapper.address.AddressWrapper.model",
+            new_callable=PropertyMock,
+        ) as mocked_model, patch(
+            "imopay_wrapper.wrapper.address.AddressWrapper.action",
+            new_callable=PropertyMock,
+        ) as mocked_action, patch(
+            "imopay_wrapper.wrapper.address.AddressWrapper._patch"
+        ) as mocked_post:
+            self.client.update("foo", {})
+
+        mocked_model.assert_called_once()
+
+        mocked_action.assert_called_once()
+
+        mocked_post.assert_called_once_with(
+            self.client._construct_url(
+                action=mocked_action.return_value,
+                subaction="update_by_name_and_uf",
+                identifier="foo",
+            ),
+            mocked_model.return_value.return_value.to_dict.return_value,
+        )
+
+    def test_get_by_document(self):
+        with patch(
+            "imopay_wrapper.wrapper.address.AddressWrapper.action",
+            new_callable=PropertyMock,
+        ) as mocked_action, patch(
+            "imopay_wrapper.wrapper.address.AddressWrapper._post"
+        ) as mocked_post:
+            self.client.get_by_document("foo")
+
+        mocked_action.assert_called_once()
+
+        expected_data = {"cpf_cnpj": "foo"}
+
+        mocked_post.assert_called_once_with(
+            self.client._construct_url(
+                action=mocked_action.return_value, subaction="get_by_document"
+            ),
+            expected_data,
+        )
