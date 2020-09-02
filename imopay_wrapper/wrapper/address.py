406f66701cd98242a5ac8653a83404c381f49357
Rodrigo Castro <rodrigondec@gmail.com>
2020-09-02 11:27:06 -0300

    adiciona método update e get_by_document

--- imopay_wrapper/wrapper/address.py
+++ imopay_wrapper/wrapper/address.py
@@ -19,3 +19,15 @@ class AddressWrapper(BaseImopayWrapper, CreateMixin, UpdateMixin, RetrieveMixin)
         instance = self.model(**data)
         url = self._construct_url(action=self.action, subaction="create_by_name_and_uf")
         return self._post(url, instance.to_dict())
+
+    def update(self, identifier, data: dict):
+        instance = self.model(**data)
+        url = self._construct_url(
+            action=self.action, subaction="update_by_name_and_uf", identifier=identifier
+        )
+        return self._patch(url, instance.to_dict())
+
+    def get_by_document(self, document):
+        data = {"cpf_cnpj": document}
+        url = self._construct_url(action=self.action, subaction="get_by_document")
+        return self._post(url, data)
